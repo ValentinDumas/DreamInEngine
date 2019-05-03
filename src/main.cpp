5de34ed9849cbc4c0f862ca5f42d02f23bb4b896
Valentin Dumas <valentin.dumas@viacesi.fr>
2019-05-03 10:41:19 +0200

    [test] template works on linu2x

--- src/main.cpp
+++ src/main.cpp
@@ -24,22 +24,6 @@ int main(int argc, char **argv)
 
     auto t_index = getTypeIndex<int>();
 
-    Complex complex(4,5);
-
-    std::cout << "complex.getImag() = " << complex.getImag() << std::endl;
-    std::cout << "complex.getReal() = " << complex.getReal() << std::endl;
-    std::cout << "complex.abs() = " << complex.abs() << std::endl;
-
-    LuaScript script("assets\\scripts\\Player.lua");
-    float posX = script.get<float>("player.position.x");
-    float posY = script.get<float>("player.position.y");
-    std::string filename = script.get<std::string>("player.filename");
-    int hp = script.get<int>("player.HP");
-
-    std::cout<<"Position X = "<<posX<<", Y = "<<posY<<std::endl;
-    std::cout<<"Filename:"<<filename<<std::endl;
-    std::cout<<"HP:"<<hp<<std::endl;
-
     utils::filesystem::get_tree(path);
 
     std::cout << "Setup status: SUCCESS !" << std::endl;
